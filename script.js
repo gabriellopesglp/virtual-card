494478db1d9d333962933a3a3263424bdf85aa33
Gabriel Lopes <gabriel.lopes_glp@outlook.com>
2024-09-04 16:00:37 -0300

    fixing script for topbar theme

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 const checkbox = document.getElementById("checkbox");
+
+function updateThemeColor() {
+  const body = document.body;
+  const isDarkTheme = body.classList.contains("dark-mode");
+  const themeColor = isDarkTheme ? "#1c1c1e" : "#f6f7f8";
+
+  let metaThemeColor = document.querySelector('meta[name="theme-color"]');
+  if (!metaThemeColor) {
+    metaThemeColor = document.createElement("meta");
+    metaThemeColor.name = "theme-color";
+    document.head.appendChild(metaThemeColor);
+  }
+  metaThemeColor.content = themeColor;
+}
+
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark-mode");
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  function updateThemeColor() {
-    const body = document.body;
-    const isDarkTheme = body.classList.contains("dark-mode");
-    const themeColor = isDarkTheme ? "#1c1c1e" : "#f6f7f8";
-
-    let metaThemeColor = document.querySelector('meta[name="theme-color"]');
-    if (!metaThemeColor) {
-      metaThemeColor = document.createElement("meta");
-      metaThemeColor.name = "theme-color";
-      document.head.appendChild(metaThemeColor);
-    }
-    metaThemeColor.content = themeColor;
-  }
-
   updateThemeColor();
-
-  checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("dark-mode");
-    updateThemeColor();
-  });
 });
